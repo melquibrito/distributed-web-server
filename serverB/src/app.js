8318804b77697951583fcc80be68352f0be2b6cd
Melqui Brito <45269246+melquibrito@users.noreply.github.com>
2020-04-21 18:51:36 -0300

    Update app.js

--- serverB/src/app.js
+++ serverB/src/app.js
@@ -1,10 +1,10 @@
 const express = require("express");
-const {HOST, PORT} = require("./conf/conf");
+const {HOST, PORT, distributer} = require("./conf/conf");
 
 const app = express();
 
 app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", 'http://127.0.0.1:8081');
+    res.header("Access-Control-Allow-Origin", distributer);
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authentication");
     next();
 });
